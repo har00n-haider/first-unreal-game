047b28ce3b0137ed91d5918f5790dee0e56762de
haroon-haider <haroon.haider@3drepo.org>
2022-02-17 08:52:44 +0000

    dunno

---
+++ Source/FirstProject/FloatingPlatform.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FloatingPlatform.generated.h"
+
+UCLASS()
+class FIRSTPROJECT_API AFloatingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloatingPlatform();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Platform")
+	UStaticMeshComponent* Mesh;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
